31d5fdcffb1f2d49474a9ded8b40732cc74b3e26
Christine Tang <tang.c.christine@gmail.com>
2020-07-13 22:40:52 -0400

    Update checker.py

--- Hello-World/checker.py
+++ Hello-World/checker.py
@@ -9,18 +9,16 @@ look = input("Who are you looking for in Cohort 1?: ")
 
 if look.capitalize() in cohort1:
     print(f"Found {look.capitalize()}!")
-    sys.exit(0)
 else:
     print(f"Did not find {look.capitalize()}. Sorry!")
-    sys.exit(1)
 
 #Implement a dictionary; key and value
 
-print("Try the phonebook!")
+print("Let's try the phonebook to look for Emma:")
 phonebook = { "Emma": "723-184-9321",
             "Christine": "638-172-1234"}
 
 if "Emma" in phonebook:
-    print(f"Emma's cell is {phonebook['Emma']}") #use single quotations in the middle, double outside
+    print(f"Emma's cell is {phonebook['Emma']}! Hope this helps.") #use single quotations in the middle, double outside
 else:
     print("Nope sorry couldn't find Emma...")
