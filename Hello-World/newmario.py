a52beed1059710de5537ea159b1f50584bd62950
Christine Tang <tang.c.christine@gmail.com>
2020-07-13 21:16:44 -0400

    Update

---
+++ Hello-World/newmario.py
@@ -0,0 +1,22 @@
+# Repeating some of the basic Mario instructions from the early C projects
+
+#Print 4 lines of ? as with the Mario blocks
+print("-----------------")
+print("Print a row of ?????")
+for i in range (4):
+    print("?", end = "") #use end = "" to stop each row from printing a new line.
+print()
+
+#Print a column of hashtags:
+print("-----------------")
+print("Print a column of hashtags:")
+for i in range (3):
+    print("#")
+
+#Print a box of # too:
+print("-----------------")
+print("Print a box of #:")
+for i in range(3):
+    for j in range(3):
+        print("#", end = "")
+    print() 
